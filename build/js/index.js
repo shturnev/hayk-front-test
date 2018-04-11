4510afcfd9deef0de7cd4cb88592e945a4cc234e
shturnev <sht_job@ukr.net>
2018-04-11 22:06:48 +0300

    сборка макета - finish

--- build/js/index.js
+++ build/js/index.js
@@ -57,37 +57,97 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 __webpack_require__(1);
 
-var yy = {
-  a: function a() {
-    console.log("что нибудь");
-  }
-};
+var _svg = __webpack_require__(11);
 
-var bb = _extends({}, yy);
+var _svg2 = _interopRequireDefault(_svg);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import Vivus from 'vivus'; //https://www.npmjs.com/package/vivus
+/*
+new Vivus('svg-logo', {duration: 200}, function () {
+  console.log('yo');
+});*/
+
+var example_one = new _svg2.default({ "elem": "#svg-logo", "increaseBy": 4, "timeout": 5 });
+// global.$ = require("jquery");
+
+example_one.animate(function (el) {
+  $("#svg-logo path").eq(el.current - 1).css({ fill: "white" });
+});
+
+$(".tlt").textillate({
+  initialDelay: 1000,
+  in: {
+    effect: 'fadeIn'
+  },
+  callback: function callback() {
+    $(".after-tlt").css({ opacity: 1 });
+  }
+});
 
 /***/ }),
-/* 1 */
+
+/***/ 1:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
+/***/ }),
+
+/***/ 11:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = SvgAnimation;
+/*https://github.com/im4aLL/svgPathAnimation @author: me@habibhadi.com*/
+function SvgAnimation(a) {
+  this.svg = $(a.elem), this.increaseBy = void 0 !== a.increaseBy ? a.increaseBy : 5, this.timeout = void 0 !== a.timeout ? a.timeout : 1, this.reverse = void 0 !== a.reverse ? a.reverse : !1, this.currentPath = 1, this.totalPath = 0, this.init();
+}SvgAnimation.prototype.animate = function (a) {
+  "function" == typeof a ? this.runAnimation(a) : this.runAnimation();
+}, SvgAnimation.prototype.init = function () {
+  var a = this.reverse;this.svg.find("path").each(function (b, c) {
+    var e = $(c),
+        f = c.getTotalLength();e.attr("stroke-dasharray", f + " " + f).attr("stroke-dashoffset", a === !0 ? -f : f);
+  });
+}, SvgAnimation.prototype.runAnimation = function (a) {
+  this.totalPath = this.svg.find("path").length;var b = { currentPath: this.currentPath, totalPath: this.totalPath, pathElem: this.svg.find("path:eq(" + (this.currentPath - 1) + ")") };this.pathAnimation(a, b);
+}, SvgAnimation.prototype.pathAnimation = function (a, b) {
+  var c = this,
+      d = b.pathElem,
+      e = parseFloat(d.attr("stroke-dashoffset")),
+      f = this.increaseBy,
+      g = setInterval(function () {
+    if (c.reverse === !0 && e >= 0 || c.reverse === !1 && 0 >= e) {
+      if (clearInterval(g), "function" == typeof a && a.call(this, { obj: d, current: b.currentPath, total: c.totalPath }), b.currentPath++, b.currentPath <= c.totalPath) {
+        var h = { currentPath: b.currentPath, pathElem: c.svg.find("path:eq(" + (b.currentPath - 1) + ")") };c.pathAnimation(a, h);
+      }
+    } else c.reverse === !0 ? (e += f, d.attr("stroke-dashoffset", e >= 0 ? 0 : e)) : (e -= f, d.attr("stroke-dashoffset", 0 >= e ? 0 : e));
+  }, this.timeout);
+};
+
 /***/ })
-/******/ ]);
+
+/******/ });
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,27 @@
 import '../styles/main.scss';
+// global.$ = require("jquery");
+import SvgAnimation from './svg.min';
 
+// import Vivus from 'vivus'; //https://www.npmjs.com/package/vivus
+/*
+new Vivus('svg-logo', {duration: 200}, function () {
+  console.log('yo');
+});*/
 
-let yy = {
-  a(){
-    console.log("что нибудь");
+var example_one = new SvgAnimation({ "elem" : "#svg-logo", "increaseBy" : 4, "timeout" : 5 });
+example_one.animate(function (el) {
+  $("#svg-logo path").eq(el.current - 1).css({fill: "white"});
+});
+
+$(".tlt").textillate({
+  initialDelay: 1000,
+  in: {
+    effect: 'fadeIn',
+  },
+  callback: function () {
+    $(".after-tlt").css({opacity: 1});
   }
-};
+});
 
-let bb = {
-    ...yy
-};
+
+
